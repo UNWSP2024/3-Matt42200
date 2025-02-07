c8ef459fd05bf6dc5efdb6a10b0285e389a60121
Matt42200 <mranderson4@students.unwsp.edu>
2025-02-07 11:43:14 -0600

    Add files via upload

---
+++ Week 3 Programs .py
@@ -0,0 +1,17 @@
+﻿BEGIN
+    DISPLAY "Enter your age: "
+    INPUT age
+
+
+    IF age >= 0 AND age <= 12 THEN
+        DISPLAY "You are a Child."
+    ELSE IF age >= 13 AND age <= 19 THEN
+        DISPLAY "You are a Teenager."
+    ELSE IF age >= 20 AND age <= 64 THEN
+        DISPLAY "You are an Adult."
+    ELSE IF age >= 65 THEN
+        DISPLAY "You are a Senior."
+    ELSE
+        DISPLAY "Invalid age entered."
+    ENDIF
+END
